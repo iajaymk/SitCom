9e402efe3f04db22e386ccf9dc82999d7f023801
Ajay Kumar <ajaykumarmkaju@gmail.com>
2023-05-19 03:26:16 +0530

    add dependencies and icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
-import logo from "./logo.png";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Mockman from "mockman-js";
+
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/mock-api" element={<Mockman colorScheme="dark"/>}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import { Header } from "../../components"
+
+export const HomePage = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
