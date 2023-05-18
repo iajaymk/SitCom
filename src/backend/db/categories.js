4923d1e085f2a3460d357dc94f29e38d9059bbe7
Ajay Kumar <ajaykumarmkaju@gmail.com>
2023-05-19 02:23:24 +0530

    feat: added categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,25 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
+    categoryName: "dining-chairs",
     description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+      "A chair designed to be used during mealtimes, at a dining table.",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "lounge-chairs",
     description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+      "A long chair with a back that can be straight for sitting on or flat for lying on.",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryName: "office-chairs",
+    description: "A chair that is designed for use at a desk in an office.",
   },
-];
+  {
+    _id: uuid(),
+    categoryName: "rocking-chairs",
+    description:
+      "A chair with two curved bands (also known as rockers) attached to the bottom of the legs, connecting the legs on each side to each other.",
+  },
+]
